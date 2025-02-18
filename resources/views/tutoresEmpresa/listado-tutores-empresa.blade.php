f4f2ef07cc2d51a97000234c1daf52a212a3feb4
david <4263344@alu.murciaeduca.es>
2025-02-18 13:41:57 +0100

    Entrega final

--- resources/views/tutoresEmpresa/listado-tutores-empresa.blade.php
+++ resources/views/tutoresEmpresa/listado-tutores-empresa.blade.php
@@ -4,12 +4,12 @@
 @section("content")
     <div class="container mx-auto px-4 py-8">
         <h1 class="text-3xl text-center font-semibold">Tutores de empresa de {{ $empresa->nombre }}</h1>
-        <div class="mr-5">
+        <div class="mr-5 container flex justify-start mb-3 w-1/6">
             <a href="{{ route("showAnyadirTutorIes") }}" class="flex items-center w-full px-4 py-2 bg-green-600 text-white font-semibold rounded-lg shadow-md hover:bg-green-800 transition duration-300">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
                 </svg>
-                Añadir Tutor IES
+                Añadir Tutor Empresa
             </a>
         </div>
 
